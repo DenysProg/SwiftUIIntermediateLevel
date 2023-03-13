97199af6a313fa829cb4ce33e30f84e4d76b7eb3
DenysProg <denysnikolaichuk@gmail.com>
2023-03-13 09:01:11 +0200

    Initial Commit

---
+++ SwiftUIIntermediateLevel/SwiftUIIntermediateLevelApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIIntermediateLevelApp.swift
+//  SwiftUIIntermediateLevel
+//
+//  Created by Denys Nikolaichuk on 13.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIIntermediateLevelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
